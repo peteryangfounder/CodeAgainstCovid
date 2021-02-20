efb3c1b70259b718c3a70e51c9e9b702a865319f
Peter Yang <peter@reviewerly.io>
2021-02-20 08:44:42 -0500

    Add homepage

---
+++ .bash_profile
@@ -0,0 +1,2 @@
+alias mongod="/c/Program\ files/MongoDB/Server/4.4/bin/mongod.exe"
+alias mongo="/c/Program\ Files/MongoDB/Server/4.4/bin/mongo.exe"
--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,15 @@ router.get('/', async (req, res) => {
 	}
 });
 
+// router.get('/', async (req, res) => {
+// 	try {
+// 		const articles = await Article.find().sort({
+// 			createdAt: -1
+// 		});
+// 		res.render('articles/index', { articles: articles });
+// 	} catch (err) {
+// 		res.render('articles/error');
+// 	}
+// });
+
 module.exports = router;
